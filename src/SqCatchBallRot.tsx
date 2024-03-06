e20c72f01a496da8f2a3e8506c9d4bfb0efa9d59
Anwesh43 <anweshthecool0@gmail.com>
2024-03-06 21:07:16 +0530

    adding SqCatchBallRot

---
+++ src/SqCatchBallRot.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useStyle } from "./hooks";
+import withContext from "./withContext";
+
+interface SqCatchBallRotProps {
+    w : number, 
+    h : number,
+    scale : number, 
+    cb : () => void ,
+    color : string 
+}
+const SqCatchBallRot : React.FC<SqCatchBallRotProps> = (props : SqCatchBallRotProps) => {
+    const {parentStyle, circleStyle, blockStyle, fixInMiddleStyle} = useStyle(props.w, props.h, props.scale, props.color)
+    return (
+        <div style = {parentStyle()}>
+            <div style = {fixInMiddleStyle(180)}>
+                <div style = {blockStyle()}>
+                </div>
+            </div>
+            <div style = {fixInMiddleStyle(0)}>
+                <div style = {circleStyle()}>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default withContext(SqCatchBallRot)
